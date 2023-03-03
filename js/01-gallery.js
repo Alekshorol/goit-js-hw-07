8ea8c771c0c335ce26231feae679ebefaf6ab184
AleksHor <horosh861@gmail.com>
2023-03-03 10:54:26 +0200

    interim commit task 7.1

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,4 +1,22 @@
-import { galleryItems } from './gallery-items.js';
+import { galleryItems } from "./gallery-items.js";
 // Change code below this line
 
 console.log(galleryItems);
+
+const galleryEl = document.querySelector(".gallery");
+console.log(galleryEl);
+
+const galleryMarkup = makeGalleryMurkup(galleryItems);
+
+galleryEl.insertAdjacentHTML("beforeend", galleryMarkup);
+
+galleryEl.addEventListener("click", atGalleryClick);
+
+function atGalleryClick(evt) {
+  evt.preventDefault();
+  const isImage = evt.target.classList.contains("gallery__image");
+  if (!isImage) {
+    return;
+  }
+  const largeImageURL = evt.target.dataset.sourse;
+}

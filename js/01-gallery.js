6b8c58d012ebb5e3dd02761095cc3c0c71712765
AleksHor <horosh861@gmail.com>
2023-03-05 17:05:07 +0200

    done task 7.2 + add close with Escape

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -19,16 +19,29 @@ function atGallery(evt) {
   const largeImage = evt.target.dataset.source;
   const instance = makeLightboxExample(largeImage);
   instance.show();
+  window.addEventListener("keydown", onBtnEscClick);
+  function onBtnEscClick(evt) {
+    console.log(evt);
+    if (evt.code === "Escape") {
+      console.log(evt);
+      instance.close();
+      window.removeEventListener("keydown", onBtnEscClick);
+    }
+  }
+  if (!instance.visible()) {
+    console.log(instance.visible());
+    window.removeEventListener("keydown", onBtnEscClick);
+  }
 }
 function makeLightboxExample(URL) {
-  return basicLightbox.create(`<img width="1280" height="800" src="${URL}">`);
+  return basicLightbox.create(`<img width="1280" height="auto" src="${URL}">`);
 }
 function makeGalleryMarkup(gallery) {
   return gallery.map((item) => makeItemMarkup(item)).join("");
 }
 function makeItemMarkup({ preview, original, description }) {
   return `<div class="gallery__item">
-    <a class="gallery__link" href="large-image.jpg">
+    <a class="gallery__link" href="${original}">
       <img
         class="gallery__image"
         src="${preview}"
--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -1,4 +1,25 @@
-import { galleryItems } from './gallery-items.js';
+import { galleryItems } from "./gallery-items.js";
 // Change code below this line
 
 console.log(galleryItems);
+
+const galleryEl = document.querySelector(".gallery");
+
+const galleryMarkup = makeGalleryMarkup(galleryItems);
+
+galleryEl.insertAdjacentHTML("beforeend", galleryMarkup);
+
+new SimpleLightbox(".gallery a", {
+  captionDelay: 250,
+  captionsData: "alt",
+});
+
+function makeGalleryMarkup(gallery) {
+  return gallery.map((item) => makeItemMarkup(item)).join("");
+}
+
+function makeItemMarkup({ original, preview, description }) {
+  return `<li><a class="gallery__item" href="${original}">
+    <img class="gallery__image" src="${preview}" alt="${description}" />
+  </a></li>`;
+}
